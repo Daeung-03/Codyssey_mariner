da2911705c69d35a3c6f5284db5cafc9ba743210
Daeung-03 <eodnd74@yonsei.ac.kr>
2025-08-08 19:46:41 +0900

    formatting que1

--- Problem2/question1.py
+++ Problem2/question1.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pickle
 
+
 def loading():
     # 파일 읽고 출력
-    with open("mars_base/Mars_Base_Inventory_List.csv", "r", encoding="utf-8") as f:
+    with open(
+        'mars_base/Mars_Base_Inventory_List.csv', 'r', encoding='utf-8'
+    ) as f:
         lines = f.readlines()
     [print(row, end='') for row in lines[1:]]
     # 파싱 -> 리스트 객체
@@ -15,11 +18,11 @@ def loading():
     log_list.sort(key=lambda x: x[4], reverse=True)
     # 별도  출력
     print('----filterd----')
-    filterd_data = [row for row in log_list if float(row[4])>=0.7]
-    [print(','.join(line)) for line in filterd_data ]
+    filterd_data = [row for row in log_list if float(row[4]) >= 0.7]
+    [print(','.join(line)) for line in filterd_data]
     # numpy 활용하여 csv파일 저장
     arr = np.array(filterd_data, dtype=str)
-    np.savetxt("Mars_Base_Inventory_danger.csv", arr, fmt='%s', delimiter=',')
+    np.savetxt('Mars_Base_Inventory_danger.csv', arr, fmt='%s', delimiter=',')
     # 이진파일 저장 후 출력
     print('----binary 출력----')
     with open('Mars_Base_Inventory_List.bin', 'wb') as f:
@@ -27,11 +30,13 @@ def loading():
 
     with open('Mars_Base_Inventory_List.bin', 'rb') as f:
         loaded_arr = pickle.load(f)
-        
-    [print(','.join(line)) for line in loaded_arr ]
+
+    [print(','.join(line)) for line in loaded_arr]
+
 
 def main():
     loading()
+
 
 if __name__ == '__main__':
     main()
