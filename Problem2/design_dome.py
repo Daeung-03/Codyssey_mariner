5a530bd26e1ebc4a8ca5994f4de563415dc197c7
Daeung-03 <eodnd74@yonsei.ac.kr>
2025-08-08 17:02:58 +0900

    done qu1

--- Problem2/design_dome.py
+++ Problem2/design_dome.py
@@ -1,7 +1,30 @@
+import numpy as np
+
 def loading():
+    # 파일 읽고 출력
     with open("mars_base/Mars_Base_Inventory_List.csv", "r", encoding="utf-8") as f:
         lines = f.readlines()
-    [print(row) for row in lines]
+    [print(row, end='') for row in lines[1:]]
+    # 파싱 -> 리스트 객체
+    log_list = list()
+    for line in lines[1:]:
+        row = line.strip().split(',')
+        log_list.append(row)
+    # 정렬
+    log_list.sort(key=lambda x: x[4], reverse=True)
+    # 별도  출력
+    print('----filterd----')
+    filterd_data = [row for row in log_list if float(row[4])>=0.7]
+    [print(','.join(line)) for line in filterd_data ]
+    # numpy 활용하여 csv파일 저장
+    arr = np.array(filterd_data, dtype=str)
+    np.savetxt("Mars_Base_Inventory_danger.csv", arr, fmt='%s', delimiter=',')
+    # 이진파일 저장 후 출력
+    print('----binary 출력----')
+    np.save('Mars_Base_Inventory_List.npy', arr)
+    loaded_arr = np.load('Mars_Base_Inventory_List.npy')
+    [print(','.join(line)) for line in loaded_arr ]
+
 
 def main():
     loading()
