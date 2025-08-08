044a143beef2b7ac06d527ada78ba9e91b4ac498
Daeung-03 <eodnd74@yonsei.ac.kr>
2025-08-08 19:45:53 +0900

    fix que1, done que2

--- Problem2/design_dome.py
+++ Problem2/design_dome.py
@@ -1,33 +1,50 @@
 import numpy as np
+import math
+import sys
 
-def loading():
-    # 파일 읽고 출력
-    with open("mars_base/Mars_Base_Inventory_List.csv", "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    [print(row, end='') for row in lines[1:]]
-    # 파싱 -> 리스트 객체
-    log_list = list()
-    for line in lines[1:]:
-        row = line.strip().split(',')
-        log_list.append(row)
-    # 정렬
-    log_list.sort(key=lambda x: x[4], reverse=True)
-    # 별도  출력
-    print('----filterd----')
-    filterd_data = [row for row in log_list if float(row[4])>=0.7]
-    [print(','.join(line)) for line in filterd_data ]
-    # numpy 활용하여 csv파일 저장
-    arr = np.array(filterd_data, dtype=str)
-    np.savetxt("Mars_Base_Inventory_danger.csv", arr, fmt='%s', delimiter=',')
-    # 이진파일 저장 후 출력
-    print('----binary 출력----')
-    np.save('Mars_Base_Inventory_List.npy', arr)
-    loaded_arr = np.load('Mars_Base_Inventory_List.npy')
-    [print(','.join(line)) for line in loaded_arr ]
+MAX_INT = sys.maxsize
 
+def sphere_area(diameter, material, thickness = 1 ):
+    check_valid(diameter, material)
+    name = {'gls': '유리', 'alu': '알루미늄', 'cars': '탄소강'}
+    density = {'gls': 2.4, 'alu': 2.7, 'cars': 7.85}
+    global area 
+    area = 3 * math.pi * ((diameter/2)**2)
+    volume = (4/3) * math.pi * (((diameter * 100)/2)**3) 
+    global weightness 
+    weightness = (volume * density[material.lower()] * 0.38)/1000
+
+    print(f"재질⇒{name[material.lower()]}, 지름⇒{diameter}, 두께⇒1, 면적⇒{area: .3f}, 무게⇒{weightness: .3f} kg")
+
+def check_valid(diameter, material):
+    if material.lower() not in ['gls', 'alu', 'cars']:
+        raise ValueError("You must choose from given values")
+    
+    if diameter <= 0:
+        raise ValueError("Diameter can't be negative and zero")
+
+    if diameter > MAX_INT:
+        raise ValueError(f"limit of diameter is {MAX_INT}")
 
 def main():
-    loading()
+    while True:
+        try:
+            print('You want to quit, press q')
+            global diameter
+            diameter = input("Enter diameter: ").strip()
+            if diameter.lower() == 'q':
+                break
+            global material
+            material = input("choose material(Gls, Alu, CarS): ").strip()
+            if material.lower() == 'q':
+                break
+            
+            sphere_area(float(diameter), material)
+            
+        except ValueError as ve:
+            print(f"Invalid diameter: {ve}")
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
---
+++ Problem2/question1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+
+def loading():
+    # 파일 읽고 출력
+    with open("mars_base/Mars_Base_Inventory_List.csv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    [print(row, end='') for row in lines[1:]]
+    # 파싱 -> 리스트 객체
+    log_list = list()
+    for line in lines[1:]:
+        row = line.strip().split(',')
+        log_list.append(row)
+    # 정렬
+    log_list.sort(key=lambda x: x[4], reverse=True)
+    # 별도  출력
+    print('----filterd----')
+    filterd_data = [row for row in log_list if float(row[4])>=0.7]
+    [print(','.join(line)) for line in filterd_data ]
+    # numpy 활용하여 csv파일 저장
+    arr = np.array(filterd_data, dtype=str)
+    np.savetxt("Mars_Base_Inventory_danger.csv", arr, fmt='%s', delimiter=',')
+    # 이진파일 저장 후 출력
+    print('----binary 출력----')
+    with open('Mars_Base_Inventory_List.bin', 'wb') as f:
+        pickle.dump(filterd_data, f, pickle.HIGHEST_PROTOCOL)
+
+    with open('Mars_Base_Inventory_List.bin', 'rb') as f:
+        loaded_arr = pickle.load(f)
+        
+    [print(','.join(line)) for line in loaded_arr ]
+
+def main():
+    loading()
+
+if __name__ == '__main__':
+    main()
